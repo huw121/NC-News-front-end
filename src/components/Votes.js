c8d151862885e15e12e505445c6b5e5ebd487f8d
Huw Jones <huwjones025@gmail.com>
2019-08-06 14:08:20 +0100

    fixed vote component

--- src/components/Votes.js
+++ src/components/Votes.js
@@ -3,23 +3,23 @@ import * as api from '../api'
 
 class Votes extends Component {
   state = {
-    votes: this.props.votes
+    voteIncrement: 0
   }
   render() {
     return (
       <div>
         <button onClick={() => { this.changeVote(1) }}>upvote</button>
-        <p>votes: {this.state.votes}</p>
+        <p>votes: {this.props.votes + this.state.voteIncrement}</p>
         <button onClick={() => { this.changeVote(-1) }}>downvote</button>
       </div>
     );
   }
 
   changeVote = (val) => {
+    this.setState(({ voteIncrement }) => ({
+      voteIncrement: voteIncrement += val
+    }));
     api.changeVote(this.props.target, this.props.id, val)
-      .then(({ article: { votes } }) => {
-        this.setState({ votes })
-      })
   }
 }
 
