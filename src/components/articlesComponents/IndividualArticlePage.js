72e017386f3abb98166033c5915b1c55aa0249f3
Huw Jones <huwjones025@gmail.com>
2019-08-06 12:02:24 +0100

    added page for individual article

--- src/components/articlesComponents/IndividualArticlePage.js
+++ src/components/articlesComponents/IndividualArticlePage.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
 import * as api from '../../api';
+import { Link } from '@reach/router';
+import Votes from '../Votes';
 
 class IndividualArticlePage extends Component {
   state = {
-    article: null
+    article: null,
+    isLoading: true
   }
 
   render() {
+    const { isLoading, article } = this.state;
+    if (isLoading) return <p>Loading...</p>
+    const { author, body, comment_count, created_at, title, topic, votes, article_id } = article;
     return (
-      <div>
-        <h1>hey</h1>
-      </div>
+      <article>
+        <Votes votes={votes} id={article_id} target="articles" />
+        <Link to={`/topics/${topic}`}><p>{topic}</p></Link>
+        <h1>{title}</h1>
+        <p>Created at: {created_at}</p>
+        <Link to={`/users/${author}`}><p>created by: {author}</p></Link>
+        <p>{body}</p>
+        <p>comments: {comment_count}</p>
+      </article>
     );
   }
 
@@ -21,7 +33,7 @@ class IndividualArticlePage extends Component {
   fetchArticle() {
     api.getData(`articles/${this.props.article_id}`)
       .then(({ article }) => {
-        this.setState({ article })
+        this.setState({ article, isLoading: false })
       })
   }
 }
