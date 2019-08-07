021dc3f7a94ee2985e7b68c1bdda8b468ab9ab18
Huw Jones <huwjones025@gmail.com>
2019-08-07 14:24:42 +0100

    changed paginator back into functional component

--- src/components/Paginator.js
+++ src/components/Paginator.js
@@ -1,21 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Paginator extends Component {
-  render() {
-    return (
-      <>
-        <button name="down" onClick={this.handlePagination}>&lt;</button>
-        <p>Page {this.props.p}</p>
-        <button name="up" onClick={this.handlePagination}>&gt;</button>
-      </>
-    );
+const Paginator = ({fetchMethod, p, pMax}) => {
+  const handlePagination = ({ target: { name } }) => {
+    if (name === "up" && p + 1 <= pMax) fetchMethod(p + 1);
+    if (name === "down" && p - 1 >= 1) fetchMethod(p - 1);
   }
 
-  handlePagination = ({ target: { name } }) => {
-    const { p, pMax } = this.props;
-    if (name === "up" && p + 1 <= pMax) this.props.fetchMethod(p + 1);
-    if (name === "down" && p - 1 >= 1) this.props.fetchMethod(p - 1);
-  }
+  return (
+    <>
+      <button name="down" onClick={handlePagination}>&lt;</button>
+      <p>Page {p}</p>
+      <button name="up" onClick={handlePagination}>&gt;</button>
+    </>
+  );
 }
 
 export default Paginator;
