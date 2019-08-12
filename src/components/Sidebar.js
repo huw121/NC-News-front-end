add022f87d7e608bc41ecd898c25f1f78995a8e6
Huw Jones <huwjones025@gmail.com>
2019-08-12 12:28:00 +0100

    a few css tweaks

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,7 +5,7 @@ import styles from './Sidebar.module.css';
 const Sidebar = ({ topics }) => {
   return (
     <nav className="sidebar">
-      <Link to="/topics"><button className={styles.sidebar}>All</button></Link>
+      <Link to="/topics"><li className={styles.sidebar}>all topics</li></Link>
       {topics.map(topic => {
         return <li key={topic.slug}><Link to={`topics/${topic.slug}`}><p className={styles.sidebar}>{topic.slug}</p></Link></li>
       })}
